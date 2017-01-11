83e8cbce9370765d41d1bcb27455f61dec2bf277
Артем <artemshabliy@gmail.com>
2017-01-11 21:27:16 +0200

    Day8

--- src/DictionariesAndMaps.java
+++ src/DictionariesAndMaps.java
@@ -16,15 +16,15 @@ public class DictionariesAndMaps {
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
-            // Write code here
             myMap.put(name, phone);
         }
 
         while(in.hasNext()){
             String s = in.next();
-            if (myMap.get(s) != null)System.out.println(s + "=" +myMap.get(s));
-            else System.out.println("Not found");
-            // Write code here
+            if (myMap.get(s) != null)
+                System.out.println(s + "=" +myMap.get(s));
+            else
+                System.out.println("Not found");
         }
         in.close();
     }
